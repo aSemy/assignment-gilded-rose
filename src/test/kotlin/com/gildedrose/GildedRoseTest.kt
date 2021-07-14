7f400d5c8f687537bfefeda64347af9dc0a09dbe
Adam Semenenko <adam.semenenko@gmail.com>
2021-07-14 11:40:47 +0200

    apply formatter

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -5,15 +5,13 @@ import org.junit.jupiter.api.Test
 
 internal class GildedRoseTest {
 
-    @Test
-    fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+  @Test
+  fun foo() {
+    val items = arrayOf<Item>(Item("foo", 0, 0))
+    val app = GildedRose(items)
+    app.updateQuality()
+    assertEquals("fixme", app.items[0].name)
 
-    }
+  }
 
 }
-
-
