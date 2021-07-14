6ec599eed15bccc95435627fb29a7c55fe0b55ca
Adam Semenenko <adam.semenenko@gmail.com>
2021-07-14 16:46:03 +0200

    - add prop-test data generators - tests for Item updates

--- src/test/kotlin/dev/adamko/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/dev/adamko/gildedrose/GildedRoseTest.kt
@@ -1,32 +1,51 @@
 package dev.adamko.gildedrose
 
+import com.gildedrose.GildedRose
+import com.gildedrose.Item
+import dev.adamko.gildedrose.testdata.ItemGens
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.comparables.shouldBeEqualComparingTo
+import io.kotest.matchers.ints.shouldBeExactly
+import io.kotest.matchers.ints.shouldBeInRange
+import io.kotest.property.Gen
+import io.kotest.property.checkAll
+import io.kotest.property.exhaustive.exhaustive
 
 class UpdateQualityTest : BehaviorSpec({
 
   Given("any item") {
     When("quality is updated") {
       Then("name should not change") {
-
+        checkAllItems(ItemGens.Names.all, ItemGens.SellIn.any, ItemGens.Quality.any) {
+          result.name shouldBeEqualComparingTo inputName
+        }
       }
     }
   }
 
   Given("any non-legendary item") {
+    val nonLegendaryItemName = ItemGens.Names.nonLegendary
     When("quality is updated") {
       Then("quality should be in range 0..50") {
-
+        checkAllItems(nonLegendaryItemName, ItemGens.SellIn.any, ItemGens.Quality.any) {
+          result.quality shouldBeInRange 0..50
+        }
       }
       Then("sellIn should change by -1") {
-
+        checkAllItems(nonLegendaryItemName, ItemGens.SellIn.any, ItemGens.Quality.any) {
+          result.sellIn shouldBeExactly (inputSellIn - 1)
+        }
       }
     }
   }
 
   Given("any legendary item") {
+    val legendaryItemName = ItemGens.Names.legendary
     When("quality is updated") {
       Then("quality should be 80") {
-
+        checkAllItems(legendaryItemName, ItemGens.SellIn.any, ItemGens.Quality.any) {
+          result.quality shouldBeExactly 80
+        }
       }
     }
   }
@@ -114,4 +133,43 @@ class UpdateQualityTest : BehaviorSpec({
     }
   }
 
-})
+}) {
+
+  companion object {
+
+    suspend inline fun checkAllItems(
+        nameGen: Gen<String>,
+        sellInGen: Gen<Int>,
+        qualityGen: Gen<Int>,
+        repetitionsGen: Gen<Int> = listOf(1).exhaustive(),
+        crossinline assert: AssertItemContext.() -> Unit
+    ) {
+      checkAll(
+          nameGen,
+          sellInGen,
+          qualityGen,
+          repetitionsGen,
+      ) { inputName: String, inputSellIn: Int, inputQuality: Int, inputRepetitions: Int ->
+        val result = createAndUpdateItem(inputName, inputSellIn, inputQuality, inputRepetitions)
+        AssertItemContext(inputName, inputSellIn, inputQuality, inputRepetitions, result).assert()
+      }
+    }
+
+    fun createAndUpdateItem(name: String, sellIn: Int, quality: Int, repetitions: Int): Item {
+      val item = Item(name, sellIn, quality)
+      val gildedRose = GildedRose(arrayOf(item));
+      repeat(repetitions) { gildedRose.updateQuality() }
+      return item
+    }
+
+    data class AssertItemContext(
+        val inputName: String,
+        val inputSellIn: Int,
+        val inputQuality: Int,
+        val inputRepetitions: Int,
+        val result: Item,
+    )
+
+  }
+
+}
---
+++ src/test/kotlin/dev/adamko/gildedrose/testdata/ItemGens.kt
@@ -0,0 +1,58 @@
+package dev.adamko.gildedrose.testdata
+
+import com.gildedrose.Item
+import dev.adamko.config.KotestExtensions.mergeAll
+import io.kotest.property.Arb
+import io.kotest.property.Gen
+import io.kotest.property.arbitrary.bind
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.map
+import io.kotest.property.arbitrary.string
+import io.kotest.property.arbitrary.withEdgecases
+import io.kotest.property.exhaustive.exhaustive
+
+object ItemGens {
+
+  object Names {
+    val regular = Arb.string().withEdgecases(
+        "+5 Dexterity Vest",
+        "Elixir of the Mongoose",
+    )
+    val aged = listOf("Aged Brie").exhaustive()
+    val tickets = listOf("Backstage passes to a TAFKAL80ETC concert").exhaustive()
+    val legendary = listOf("Sulfuras, Hand of Ragnaros").exhaustive()
+
+    val conjured = listOf(regular, aged, tickets, legendary).mergeAll().map { "Conjured $it" }
+
+    val all = listOf(regular, aged, tickets, conjured, legendary).mergeAll()
+    val nonLegendary = listOf(regular, aged, tickets, conjured).mergeAll()
+  }
+
+  object SellIn {
+    // note- ignore integer overflow for now
+    val nonExpired = Arb.int(1, Int.MAX_VALUE - 1)
+    val expired = Arb.int(Int.MIN_VALUE + 1, 0)
+    val any = Arb.int(Int.MIN_VALUE + 1 until Int.MAX_VALUE)
+  }
+
+  object Quality {
+    val regularValidRange = 0..50
+    val legendaryValidRange = 80..80
+
+    val regular = Arb.int(regularValidRange)
+
+    val any = Arb.int(Int.MIN_VALUE + 1 until Int.MAX_VALUE)
+
+  }
+
+  object Binds {
+    fun itemArb(
+        nameGen: Gen<String> = Names.all,
+        sellInGen: Gen<Int> = SellIn.any,
+        qualityGen: Gen<Int> = Quality.any,
+    ) = Arb.bind(nameGen, sellInGen, qualityGen) { name, sellIn, quality ->
+      Item(name, sellIn, quality)
+    }
+  }
+
+}
